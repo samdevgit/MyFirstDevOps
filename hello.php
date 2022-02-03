c65dffb82f697bac223c9fb67bad065e2a945f60
samdevgit <60170140+samdevgit@users.noreply.github.com>
2022-02-03 04:21:50 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,6 +3,6 @@
   <title>Sam Devops Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Fareeth!!!! This is Git!!! are you ok babay !!!</p>'; ?> 
+ <?php echo '<p>Hello Fareeth!!!! This is Git!!! are you ok babay !!! samuel Raju devops lead</p>'; ?> 
  </body>
 </html>
